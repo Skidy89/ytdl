a95cf00922023d008c40b65813e8424896e6acff
hiudyy <hiudyyy@gmail.com>
2024-12-15 12:09:26 -0300

    .

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ const { execFile } = require("child_process");
 const fs = require("fs");
 const path = require("path");
 const { Innertube, UniversalCache } = require("youtubei.js");
+const ytdl = require('@distube/ytdl-core');
 const { existsSync, mkdirSync, createWriteStream, unlinkSync } = require("fs");
 
 const ytdlpPath = path.join(__dirname, "../bin/yt-dlp");
 const cookiesPath = path.join(__dirname, "../bin/cookies.txt");
+const cookiesPath2 = path.join(__dirname, "../bin/cookies.txt");
+const cookiesJson = JSON.parse(fs.readFileSync(cookiesPath2, 'utf-8'))
 const tempPath = path.join(__dirname, "../temp");
+const formattedCookies = cookiesJson.map(cookie => `${cookie.name}=${cookie.value}`).join('; ');
+
+const agentOptions = {
+  pipelining: 5,
+  maxRedirections: 0,
+};
+
+const agent = ytdl.createAgent(cookiesJson, agentOptions);
+
 
 function generateRandomName(extension) {
   const timestamp = Date.now();
@@ -88,83 +100,73 @@ async function ytvdl(input) {
 }
 
 async function ytadlV2(input) {
-  const yt = await Innertube.create({
-    cache: new UniversalCache(false),
-    generate_session_locally: true,
-    cookies: fs.readFileSync(cookiesPath, 'utf-8'),
-    client: "WEB",
-});
-
-  const url = input.startsWith("http") ? `${input.split('?')[0].split('/').pop()}` : `${input}`;
+  const url = input.startsWith("http") ? input : `https://www.youtube.com/watch?v=${input}`;
   
-  const stream = await yt.download(url, {
-    type: "audio",
-    quality: "best",
+  const stream = ytdl(url, {
+    filter: "audioonly",
+    quality: "highestaudio",
     format: "m4a",
+    agent: agent,
+    requestOptions: {
+      headers: {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+        "Cookie": formattedCookies,
+      },
+    },
   });
 
   const tempFile = path.join(tempPath, generateRandomName("m4a"));
 
-  if (!existsSync(tempPath)) {
-    mkdirSync(tempPath);
+  if (!fs.existsSync(tempPath)) {
+    fs.mkdirSync(tempPath);
   }
 
-  const file = createWriteStream(tempFile);
-  for await (const chunk of Utils.streamToIterable(stream)) {
-    file.write(chunk);
-  }
-  file.end();
+  const file = fs.createWriteStream(tempFile);
 
   return new Promise((resolve, reject) => {
+    stream.pipe(file);
+    stream.on("error", (err) => reject(`Error downloading stream: ${err.message}`));
     file.on("finish", () => {
-      const buffer = require("fs").readFileSync(tempFile);
-      unlinkSync(tempFile);
+      const buffer = fs.readFileSync(tempFile);
+      fs.unlinkSync(tempFile);
       resolve(buffer);
     });
-
-    file.on("error", (err) => {
-      reject(`Error writing file: ${err.message}`);
-    });
+    file.on("error", (err) => reject(`Error writing file: ${err.message}`));
   });
 }
 
 async function ytvdlV2(input) {
-  const yt = await Innertube.create({
-    cache: new UniversalCache(false),
-    generate_session_locally: true,
-    cookies: fs.readFileSync(cookiesPath, 'utf-8'),
-    client: "WEB",
-  });
-
-  const url = input.startsWith("http") ? `${input.split('?')[0].split('/').pop()}` : `${input}`;
+  const url = input.startsWith("http") ? input : `https://www.youtube.com/watch?v=${input}`;
   
-  const stream = await yt.download(url, {
-    type: "video+audio",
-    quality: "480p",
+  const stream = ytdl(url, {
+    quality: "highest",
     format: "mp4",
+    agent: agent,
+    requestOptions: {
+      headers: {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+        "Cookie": formattedCookies,
+      },
+    },
   });
 
   const tempFile = path.join(tempPath, generateRandomName("mp4"));
 
-  if (!existsSync(tempPath)) {
-    mkdirSync(tempPath);
+  if (!fs.existsSync(tempPath)) {
+    fs.mkdirSync(tempPath);
   }
 
-  const file = createWriteStream(tempFile);
-  for await (const chunk of Utils.streamToIterable(stream)) {
-    file.write(chunk);
-  }
-  file.end();
+  const file = fs.createWriteStream(tempFile);
 
   return new Promise((resolve, reject) => {
+    stream.pipe(file);
+    stream.on("error", (err) => reject(`Error downloading stream: ${err.message}`));
     file.on("finish", () => {
       const buffer = fs.readFileSync(tempFile);
-      unlinkSync(tempFile);
+      fs.unlinkSync(tempFile);
       resolve(buffer);
     });
-    file.on("error", (err) => {
-      reject(`Error writing file: ${err.message}`);
-    });
+    file.on("error", (err) => reject(`Error writing file: ${err.message}`));
   });
 }
 
